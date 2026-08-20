4486eae69175181c30cbecf53df7dd0330369fb0
Kanishk <Kanishkgupta2003@outlook.com>
2026-08-20 17:21:00 +0530

    ﻿Add interpolation examples and limitations to app.html; define x and y in app.ts

--- Topic-1-Introduction-to-Angular/src/app/app.ts
+++ Topic-1-Introduction-to-Angular/src/app/app.ts
@@ -9,6 +9,8 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('Topic-1 Introduction to Angular');
   readonly name = signal('Kanishk');
+  public x = 10;
+  public y = 20;
   hello() {
     let name = this.name();
     console.log(`Hello ${name}`);
