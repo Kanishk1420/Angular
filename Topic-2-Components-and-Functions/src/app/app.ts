fb863ca948ac9940eadb984d43f8a065af123136
Kanishk <Kanishkgupta2003@outlook.com>
2026-08-21 16:49:00 +0530

    Made Profile Custom Component

--- Topic-2-Components-and-Functions/src/app/app.ts
+++ Topic-2-Components-and-Functions/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
+import { Nav } from './nav/nav';
+import { Profile } from './profile/profile';
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, Nav, Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
