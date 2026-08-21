64220058430b24b443a1b1b0ccc8e41e8c655175
Kanishk <Kanishkgupta2003@outlook.com>
2026-08-21 15:17:19 +0530

    Added Component in Topic 2nd

---
+++ Topic-2-Components-and-Functions/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { Header } from './header/header';
+@Component({
+  selector: 'app-root',
+  imports: [Header],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Topic-2-Components-and-Functions');
+}
