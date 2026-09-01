23a4b119ad9a2a7d23b805529cc835b06b905a5b
Kanishk <Kanishkgupta2003@outlook.com>
2026-09-01 18:30:24 +0530

    Styling in Angular

--- Topic-3-Events-and-Functions/src/app/app.ts
+++ Topic-3-Events-and-Functions/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component} from '@angular/core';
 import { Counter } from './counter/counter';
 import { Events } from './events/events';
 import { Getandset } from './getandset/getandset';
+import { Styles } from './styles/styles'
 
 
 @Component({
   selector: 'app-root',
-  imports: [Counter, Events, Getandset],
+  imports: [Counter, Events, Getandset, Styles],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ Topic-3-Events-and-Functions/src/app/styles/styles.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-styles',
+  imports: [],
+  templateUrl: './styles.html',
+  styleUrls: ['./styles.css','../theme.css'], // if you want to apply multiple styles then use styleUrls
+})
+export class Styles {}
