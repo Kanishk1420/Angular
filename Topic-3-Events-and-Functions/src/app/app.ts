5fecb055824b07497966f3b5744493477a69e36a
Kanishk <Kanishkgupta2003@outlook.com>
2026-09-01 19:03:09 +0530

    If-Else in Angular though Html

--- Topic-3-Events-and-Functions/src/app/app.ts
+++ Topic-3-Events-and-Functions/src/app/app.ts
@@ -3,11 +3,12 @@ import { Counter } from './counter/counter';
 import { Events } from './events/events';
 import { Getandset } from './getandset/getandset';
 import { Styles } from './styles/styles'
+import { IfElse } from './if-else/if-else';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Counter, Events, Getandset, Styles],
+  imports: [Counter, Events, Getandset, Styles, IfElse],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
