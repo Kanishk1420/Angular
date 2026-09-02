0da5f31b4b52ef2ca68d9e82e3d076c948ff7748
Kanishk <Kanishkgupta2003@outlook.com>
2026-09-02 17:38:16 +0530

    Switch Case

--- Topic-3-Events-and-Functions/src/app/app.ts
+++ Topic-3-Events-and-Functions/src/app/app.ts
@@ -4,11 +4,12 @@ import { Events } from './events/events';
 import { Getandset } from './getandset/getandset';
 import { Styles } from './styles/styles'
 import { IfElse } from './if-else/if-else';
+import { Switch } from './switch/switch';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Counter, Events, Getandset, Styles, IfElse],
+  imports: [Counter, Events, Getandset, Styles, IfElse, Switch],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
