0ed4786db84cf05299df416043000b058549550b
Kanishk <Kanishkgupta2003@outlook.com>
2026-09-08 18:43:00 +0530

    Data and Value Types with Signals

--- Topic-4-Signals-and-Types/src/app/data-types-with-signals/data-types-with-signals.ts
+++ Topic-4-Signals-and-Types/src/app/data-types-with-signals/data-types-with-signals.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, Signal, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-data-types-with-signals',
@@ -7,8 +7,19 @@ import { Component, signal } from '@angular/core';
   styleUrl: './data-types-with-signals.css',
 })
 export class DataTypesWithSignals {
-  data = signal<number | string | unknown | undefined | null>(20)
-  updateSignal(){
-  this.data.set("Hello");  
+  // Uses a generic type parameter to allow multiple value types
+  data = signal<number | string | undefined | null>(20); 
+
+  //Explicit type annotation defining a signal that is both readable and writable
+  withdata: WritableSignal<number | string | undefined | null> = signal(20); 
+
+  //Computed signals are read-only wrappers (Signal interface)
+  count: Signal<number> = computed(() => 10); 
+
+  updateSignal() {
+    this.data.set('Hello');
+    this.withdata.set('Hello');
+    // this.count.set(80); 
+    // This line is commented out because .set() does not exist on read-only computed signals
   }
 }
