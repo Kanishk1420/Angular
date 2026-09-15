41a4544fd23e6a1650032f0805fde2588e9b13bb
Kanishk <Kanishkgupta2003@outlook.com>
2026-09-15 15:56:10 +0530

    Two Way Binding in Angular

--- Topic-5-Contextual-Variables-and-Two-way-Binding/src/app/app.ts
+++ Topic-5-Contextual-Variables-and-Two-way-Binding/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Loops } from "./loops/loops";
+import { TwoWayBinding } from './two-way-binding/two-way-binding';
 
 @Component({
   selector: 'app-root',
-  imports: [Loops],
+  imports: [Loops, TwoWayBinding],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
