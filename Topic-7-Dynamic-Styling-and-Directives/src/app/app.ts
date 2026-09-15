934244d790b86a8c6c81a3624a820bc91fd261cf
Kanishk <Kanishkgupta2003@outlook.com>
2026-09-15 16:53:11 +0530

    Starting Dynamic Styling

---
+++ Topic-7-Dynamic-Styling-and-Directives/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Topic-7-Dynamic-Styling-and-Directives');
+}
