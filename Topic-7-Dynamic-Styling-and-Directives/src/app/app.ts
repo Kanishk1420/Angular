5d154f1acb7647143b9fb8137044c13e03e030de
Kanishk <Kanishkgupta2003@outlook.com>
2026-09-18 16:09:33 +0530

    Small Project

--- Topic-7-Dynamic-Styling-and-Directives/src/app/app.ts
+++ Topic-7-Dynamic-Styling-and-Directives/src/app/app.ts
@@ -3,10 +3,11 @@ import { DynamicStyling } from './dynamic-styling/dynamic-styling';
 import { DirectivesNgfor } from './directives-ngfor/directives-ngfor';
 import { DirectivesNgif } from './directives-ngif/directives-ngif';
 import { DirectivesNgswitch } from './directives-ngswitch/directives-ngswitch';
+import { Practice } from './practice/practice';
 
 @Component({
   selector: 'app-root',
-  imports: [DynamicStyling, DirectivesNgfor, DirectivesNgif, DirectivesNgswitch],
+  imports: [DynamicStyling, DirectivesNgfor, DirectivesNgif, DirectivesNgswitch, Practice],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
